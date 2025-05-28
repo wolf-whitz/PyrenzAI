c9a8696af254ec6837ee81c0b3f2eb0542569b49
Whitzscott (Developer) <whitzscott@gmail.com>
2025-05-29 01:38:34 +0800

    feat: update Electron main script to load production URL and configure build settings in package.json

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,4 @@
 import { app, BrowserWindow } from 'electron'
-import path from 'path'
 
 let mainWindow: BrowserWindow | null = null
 
@@ -8,20 +7,11 @@ const createWindow = () => {
     width: 1280,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true
     }
   })
 
-  const devURL = 'http://localhost:5173' 
-  const prodURL = `file://${path.join(__dirname, '../build/index.html')}`
-
-  if (process.env.NODE_ENV === 'development') {
-    mainWindow.loadURL(devURL)
-    mainWindow.webContents.openDevTools()
-  } else {
-    mainWindow.loadURL(prodURL)
-  }
+  mainWindow.loadURL('https://pyrenzai.com')
 
   mainWindow.on('closed', () => (mainWindow = null))
 }
